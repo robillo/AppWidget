35af578fd4af6d31473071e09147c6416d3356a9
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-04-12 21:46:29 +0530

    Working!!! BooYah! B) B).

--- app/src/main/java/com/appbusters/robinkamboj/appwidget/NewAppWidget.java
+++ app/src/main/java/com/appbusters/robinkamboj/appwidget/NewAppWidget.java
@@ -1,8 +1,11 @@
 package com.appbusters.robinkamboj.appwidget;
 
+import android.annotation.SuppressLint;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.widget.RemoteViews;
 
 /**
@@ -10,26 +13,32 @@ import android.widget.RemoteViews;
  */
 public class NewAppWidget extends AppWidgetProvider {
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
-
-//        CharSequence widgetText = context.getString(R.string.appwidget_text);
-
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
-//        views.setTextViewText(R.id.appwidget_text, widgetText);
-
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
-    }
-
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+        for (int widgetId : appWidgetIds) {
+            RemoteViews mView = initViews(context, appWidgetManager, widgetId);
+            appWidgetManager.updateAppWidget(widgetId, mView);
         }
     }
+
+    @SuppressWarnings("deprecation")
+    @SuppressLint("NewApi")
+    private RemoteViews initViews(Context context,
+                                  AppWidgetManager widgetManager, int widgetId) {
+
+        RemoteViews mView = new RemoteViews(context.getPackageName(),
+                R.layout.new_app_widget);
+
+        Intent intent = new Intent(context, RemoteViewService.class);
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId);
+
+        intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+        mView.setRemoteAdapter(widgetId, R.id.widgetCollectionList, intent);
+
+        return mView;
+    }
+
 
     @Override
     public void onEnabled(Context context) {
