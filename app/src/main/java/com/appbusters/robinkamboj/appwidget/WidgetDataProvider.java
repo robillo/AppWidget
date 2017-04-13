f8089bcb6cf66ecc8e0d8c9a98b4e39eb79244fc
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-04-13 17:21:49 +0530

    Working!!! BooYah! B) B).

--- app/src/main/java/com/appbusters/robinkamboj/appwidget/WidgetDataProvider.java
+++ app/src/main/java/com/appbusters/robinkamboj/appwidget/WidgetDataProvider.java
@@ -2,7 +2,6 @@ package com.appbusters.robinkamboj.appwidget;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
 
@@ -11,7 +10,8 @@ import java.util.List;
 
 public class WidgetDataProvider implements RemoteViewsService.RemoteViewsFactory {
 
-    List<String> mCollections = new ArrayList();
+    List<String> mCollections = new ArrayList<>();
+    List<String> mCollections2 = new ArrayList<>();
     private Context context;
 
     public WidgetDataProvider(Context context, Intent intent) {
@@ -40,9 +40,9 @@ public class WidgetDataProvider implements RemoteViewsService.RemoteViewsFactory
 
     @Override
     public RemoteViews getViewAt(int position) {
-        RemoteViews mView = new RemoteViews(context.getPackageName(), android.R.layout.simple_list_item_1);
-        mView.setTextViewText(android.R.id.text1, mCollections.get(position));
-        mView.setTextColor(android.R.id.text1, Color.BLACK);
+        RemoteViews mView = new RemoteViews(context.getPackageName(), R.layout.row_layout);
+        mView.setTextViewText(R.id.word, mCollections.get(position));
+        mView.setTextViewText(R.id.meaning, mCollections2.get(position));
         return mView;
     }
 
@@ -68,8 +68,10 @@ public class WidgetDataProvider implements RemoteViewsService.RemoteViewsFactory
 
     private void initData() {
         mCollections.clear();
+        mCollections2.clear();
         for (int i = 1; i <= 10; i++) {
-            mCollections.add("ListView item " + i);
+            mCollections.add("Word item " + i);
+            mCollections2.add("Meaning item" + i);
         }
     }
 }
